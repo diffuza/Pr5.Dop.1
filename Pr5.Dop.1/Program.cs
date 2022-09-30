3418059513e096232bce5f39ef6430be3c74044d
USER <USER@DESKTOP-TRQG01D>
2022-09-30 18:18:07 +0300

    Добавьте файлы проекта.

---
+++ Pr5.Dop.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom"; int age = 33; // присвоение строкового типа "string" локальной переменной "name"; присвоение целого типа "int" локальной переменной "age".
+            bool isEmployed = false; double weight = 78.65; //присвоение логического типа "bool" лок.переменной "isEmployed" со значением "false"; присвоение вещественного типа "double" лок.переменной "weight"
+            Console.WriteLine($"Имя: {name}"); Console.WriteLine($"Возраст: {age}"); //Вывод на экран Имени и возраста, указанных выше.
+            Console.WriteLine($"Вес: {weight}"); Console.WriteLine($"Работает: {isEmployed}"); //Вывод на экран Веса и условия работоспособности, указанных выше.
+        }
+    }
+}
